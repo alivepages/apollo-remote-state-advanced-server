f050b6a383d960fedf23d52b438dd08c16780142
Joel Fagundo <joel.fagundo@gmail.com>
2020-10-30 12:37:18 -0600

    fix config

--- dist_r/modules/todos/repos/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const InMemoryTodoRepo_1 = require("./implementations/InMemoryTodoRepo");
-const todosRepo = new InMemoryTodoRepo_1.InMemoryTodoRepo();
-exports.todosRepo = todosRepo;
-//# sourceMappingURL=index.js.map
